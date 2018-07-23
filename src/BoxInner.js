8f64365b76461bcf5c32ea0bdb3eb0524e73f5a8
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-07-23 13:56:32 -0500

    Split title into separate component

--- src/BoxInner.js
+++ src/BoxInner.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react"
+import BoxTitle from "./Title"
 
 export default class BoxInner extends Component {
   render() {
     return (
 
       <div className={this.props.boxClass + " " + this.props.theme}>
-        <section>{this.props.theme}</section>
+        <BoxTitle>{this.props.theme}</BoxTitle>
         <button className={this.props.theme}>A button</button>
       </div>
     )
---
+++ src/BoxTitle.js
@@ -0,0 +1,8 @@
+import React, {Component} from "react"
+
+export default class Title extends Component {
+
+  render() {
+    return <h4>{this.props.children}</h4>
+  }
+}
